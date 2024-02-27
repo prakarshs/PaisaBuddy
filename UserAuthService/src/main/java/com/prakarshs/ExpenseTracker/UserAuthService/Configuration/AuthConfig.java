d402e584971e61fb4c71261131640f0754209c97
jhethia <jhethia@gmail.com>
2024-02-27 12:54:39 +0530

    added security configurations

---
+++ UserAuthService/src/main/java/com/prakarshs/ExpenseTracker/UserAuthService/Configuration/AuthConfig.java
@@ -0,0 +1,26 @@
+package com.prakarshs.ExpenseTracker.UserAuthService.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AuthConfig {
+
+    @Bean
+    private PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    private SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeHttpRequests->authorizeHttpRequests.anyRequest().permitAll()).build();
+    }
+}
